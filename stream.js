89d1575efd81a8d91414fb95bfee1c09b998697a
poorpeople-cmd <poorpeople5991079@gmail.com>
2026-07-07 17:12:36 +0500

    Update stream.js

--- stream.js
+++ stream.js
@@ -44,7 +44,7 @@ const NO_REFRESH_DOMAINS = [
     'youtube.com',
     'facebook.com',
     'streamed.pk',
-    'cricstreams.',
+    'cricstreams.org', // crichd
     'website-vercel-helper-d-jaja-3-2.vercel.app',
     'websitestream.netlify.app/?ch=Channel%20HD%2071'
 ];
