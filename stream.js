735b49905a54d73e3851815c0ca3d987b7b3684f
poorpeople-cmd <poorpeople5991079@gmail.com>
2026-07-09 19:15:02 +0500

    Update stream.js

--- stream.js
+++ stream.js
@@ -34,7 +34,7 @@ const obs = new OBSWebSocket();
 // =========================================================================================
 // ⏱️ BIG VARIABLE: FORCE AUTO-REFRESH TIME (IN MINUTES)
 // =========================================================================================
-const FORCE_REFRESH_MINUTES = 9; 
+const FORCE_REFRESH_MINUTES = 40; 
 const FORCE_REFRESH_MS = FORCE_REFRESH_MINUTES * 60 * 1000;
 
 // =========================================================================================
